44695219fec58ff5aedf59e6c1fc46e2c8bb1347
sai <obilisainikhil@gmail.com>
2026-08-25 09:20:08 +0530

    Fix base path for GitHub Pages deployment

--- frotend/vite.config.ts
+++ frotend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CropMate-2/',
+  base: '/Agri-Rent-2/',
   plugins: [react()],
   server: {
     proxy: {
